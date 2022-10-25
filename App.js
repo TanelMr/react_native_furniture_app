556ac166e062fb767e912e6bb7288284c0068188
Tanel Märjama <tanel.marjama@khk.ee>
2022-10-25 10:36:15 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Splash from "./src/screens/auth/Splash";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Splash/>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, Image, Pressable } from "react-native";
+import { styles } from './styles';
+
+import Button from "../../../components/Button";
+
+const Splash = () => {
+  const onSignup = () => {
+    console.log('Signup');
+  };
+
+  const onSignin = () => {
+    console.log('Signin');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image resizeMode='contain' style={styles.image} source={require('../../../assets/splash_image.png')} />
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>You'll Find</Text>
+        <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
+        <Text style={styles.title}>Here!</Text>
+      </View>
+
+      <View style={styles.buttonCont}>
+        <Button onPress={onSignup} title='Sign Up' />
+      </View>
+
+      <Pressable onPress={onSignin} hitSlop={10}>
+        <Text style={styles.footerText}>Sign In</Text>
+      </Pressable>
+    </View>
+
+  );
+};
+
+export default Splash;
